29f3dba7e499bda08a64ffec9df0e2683e6d8780
Ubuntu <ubuntu@ip-172-31-13-146.us-west-2.compute.internal>
2017-06-06 03:08:06 +0000

    scraped stock data from google

--- predicting_stock_market_trends_with_Twitter/lib/__init__.py
+++ predicting_stock_market_trends_with_Twitter/lib/__init__.py
@@ -1,20 +1,28 @@
 import numpy as np
 import pandas as pd
+import pandas_datareader as pdr
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import sys
+
+from datetime import datetime
 
 #get rid of missing font warning when plotting:
 from matplotlib import rcParams
+
+from matplotlib import style
+style.use('ggplot')
+
 #from lib.helper_database import 
 from lib.helper_system import suppress_warnings
 
 rcParams['font.family'] = 'Droid Sans'
 
 #__all__ makes these names available in Jupyter notebook:
-__all__ = ['np',
+__all__ = ['datetime',
+           'np',
            'pd',
+           'pdr',
            'plt',
            'rcParams',
            'sns',
